89e0e05e394d5a8576dbae83e08fbec1dbe622c2
Michael Smith <m@michaelsmith.be>
2016-04-01 15:08:03 +0200

    Hotfix: Use rolling 30 days period

--- app/models/average_daily_measurement.rb
+++ app/models/average_daily_measurement.rb
@@ -6,7 +6,7 @@ class AverageDailyMeasurement < ActiveRecord::Base
   self.primary_key = "id"
   belongs_to :user
 
-  scope :this_month, -> { where(daily_timestamp: Date.today.beginning_of_month.to_time..Time.now) }
+  scope :this_month, -> { where(daily_timestamp: 30.days.ago.beginning_of_day..Time.now) }
 
   def chart_datetime
     daily_timestamp.to_i * 1000 if daily_timestamp
--- app/models/average_hourly_measurement.rb
+++ app/models/average_hourly_measurement.rb
@@ -6,7 +6,7 @@ class AverageHourlyMeasurement < ActiveRecord::Base
   self.primary_key = "id"
   belongs_to :user
 
-  scope :this_month, -> { where(hourly_timestamp: Date.today.beginning_of_month.to_time..Time.now) }
+  scope :this_month, -> { where(hourly_timestamp: 30.days.ago.beginning_of_day..Time.now) }
 
   def chart_datetime
     hourly_timestamp.to_i * 1000 if hourly_timestamp
