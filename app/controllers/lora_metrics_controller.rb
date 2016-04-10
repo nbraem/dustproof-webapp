1d3d9d0d48a77d342e26f67da84a43ce708ad283
Michael Smith <m@michaelsmith.be>
2016-04-10 19:58:29 +0200

    Hotfix: filter Lora metrics on current user's device eui

--- app/controllers/lora_metrics_controller.rb
+++ app/controllers/lora_metrics_controller.rb
@@ -2,6 +2,6 @@ class LoraMetricsController < ApplicationController
   respond_to :html
 
   def index
-    @lora_metrics = LoraMetric.order("hourly_timestamp ASC")
+    @lora_metrics = LoraMetric.where(device_eui: current_user.device_eui).order("hourly_timestamp ASC")
   end
 end
