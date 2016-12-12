7a8d09699ea29e2a8c270c6b523fb214a5fcdaff
Michael Smith <m@michaelsmith.be>
2016-12-12 01:26:18 +0100

    Hotfix: Adds chart with plain, unfiltered P1, P2 and PM2.5 ratios.

--- db/migrate/20160315125225_create_average_daily_measurements_view.rb
+++ db/migrate/20160315125225_create_average_daily_measurements_view.rb
@@ -3,23 +3,29 @@ class CreateAverageDailyMeasurementsView < ActiveRecord::Migration
     execute <<-SQL
       CREATE VIEW average_daily_measurements AS
         SELECT row_number() OVER () AS id,
-               user_id,
-               round(avg(temperature)::numeric, 2) AS average_temperature,
-               round(avg(humidity)::numeric, 2) AS average_humidity,
-               round(avg(p1_concentration)) AS average_p1_concentration,
-               round(avg(p2_concentration)) AS average_p2_concentration,
-               round(avg(p1_concentration_filtered)) AS average_p1_concentration_filtered,
-               round(avg(p2_concentration_filtered)) AS average_p2_concentration_filtered,
-               date_trunc('day', timestamp) AS daily_timestamp
-            FROM measurements
-              WHERE temperature IS NOT NULL
-                AND humidity IS NOT NULL
-                AND p1_concentration IS NOT NULL
-                AND p2_concentration IS NOT NULL
-                AND p1_concentration_filtered IS NOT NULL
-                AND p2_concentration_filtered IS NOT NULL
-              GROUP BY daily_timestamp, user_id
-              ORDER BY daily_timestamp;
+          user_id,
+          round(avg(temperature)::numeric, 2) AS average_temperature,
+          round(avg(humidity)::numeric, 2) AS average_humidity,
+          avg(p1_ratio) AS average_p1_ratio,
+          avg(p2_ratio) AS average_p2_ratio,
+          avg(pm25_ratio) AS average_pm25_ratio,
+          round(avg(p1_concentration)) AS average_p1_concentration,
+          round(avg(p2_concentration)) AS average_p2_concentration,
+          round(avg(p1_concentration_filtered)) AS average_p1_concentration_filtered,
+          round(avg(p2_concentration_filtered)) AS average_p2_concentration_filtered,
+          date_trunc('day', timestamp) AS daily_timestamp
+        FROM measurements
+          WHERE temperature IS NOT NULL
+            AND humidity IS NOT NULL
+            AND p1_ratio IS NOT NULL
+            AND p2_ratio IS NOT NULL
+            AND pm25_ratio IS NOT NULL
+            AND p1_concentration IS NOT NULL
+            AND p2_concentration IS NOT NULL
+            AND p1_concentration_filtered IS NOT NULL
+            AND p2_concentration_filtered IS NOT NULL
+          GROUP BY daily_timestamp, user_id
+          ORDER BY daily_timestamp;
     SQL
   end
 
