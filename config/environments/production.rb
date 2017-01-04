9ae4d5e29598568e315a4a5bfc6521c12e9c9af5
Michael Smith <m@michaelsmith.be>
2017-01-04 03:05:42 +0100

    Hotfix: Switched to more reliable own mailserver

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,9 @@ Rails.application.configure do
   # Mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
+    address: "mail.vlotmail.be",
+    port: 25,
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.sendgrid["user_name"],
-    password: Rails.application.secrets.sendgrid["password"],
-    authentication: :login,
     domain: "dustproof.be"
   }
   config.action_mailer.perform_deliveries = true
