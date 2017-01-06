378ca38013bc3cba59a0a5fb07d473a7a64ea7fe
Michael Smith <m@michaelsmith.be>
2017-01-06 14:28:19 +0100

    Cleanup: removed obsolete migrations.

--- db/migrate/20170106131619_drop_calculated_fields.rb
+++
@@ -1,8 +0,0 @@
-class DropCalculatedFields < ActiveRecord::Migration
-  def change
-    remove_column :measurements, :p1_concentration
-    remove_column :measurements, :p1_concentration_filtered
-    remove_column :measurements, :p2_concentration
-    remove_column :measurements, :p2_concentration_filtered
-  end
-end
