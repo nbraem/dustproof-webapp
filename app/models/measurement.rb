7131c565595e360f33b361c8504c11a67221d40c
Michael Smith <m@michaelsmith.be>
2017-01-19 15:24:34 +0100

    Hotfix: only consider dust measurement errors for invalid measurements

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -11,16 +11,10 @@ class Measurement < ActiveRecord::Base
   private
 
   def validate_measurement
-    begin
-      if (self.p1_ratio < self.p2_ratio) ||
-          (self.temperature < -40.0 || self.temperature > 125.0) ||
-          (self.humidity < 0.0 || self.humidity > 100.0)
-        self.is_valid = false
-      else
-        self.is_valid = true
-      end
-    rescue
+    if self.p1_ratio < self.p2_ratio
       self.is_valid = false
+    else
+      self.is_valid = true
     end
     return true
   end
