28503045dee8c3cbdd54d445f3cd8be2d1dc8e9d
Michael Smith <m@michaelsmith.be>
2017-01-24 17:09:27 +0100

    Added nice dropdown for selecting device eui on lora metrics view

--- app/controllers/lora_metrics_controller.rb
+++ app/controllers/lora_metrics_controller.rb
@@ -6,5 +6,7 @@ class LoraMetricsController < ApplicationController
     @q.sorts = "hourly_timestamp asc"
     @q.device_eui_eq = "001AF081" if @q.device_eui_eq.blank?
     @lora_metrics = @q.result
+
+    @device_euis = IncomingMessage.select(:device_eui).order(device_eui: :asc).uniq.collect(&:device_eui)
   end
 end
--- spec/factories/incoming_messages.rb
+++ spec/factories/incoming_messages.rb
@@ -5,23 +5,23 @@ FactoryGirl.define do
     status "new"
     timestamp Time.now
 
-    body '{"gatewayEui":"GATEWAY_EUI","deviceEui":"DEVICE_EUI",'\
+    body '{"gatewayEui":"GATEWAY_EUI","devAddr":"DEVICE_EUI",'\
             '"packetTime":"2016-03-14T11:51:19.783817Z","tmst":3969010612,'\
             '"frequency":868.1,"dataRate":"SF12BW125","rssi":-120,'\
-            '"snr":-14.2,"data":"FfQAeAAZAF8ADQAEAE4CxA=="}'
+            '"snr":-14.2,"payload":"FfQAeAAZAF8ADQAEAE4CxA=="}'
 
     trait :with_lora_data_from_different_device_eui do
-      body '{"gatewayEui":"GATEWAY_EUI","deviceEui":"ANOTHER_DEVICE_EUI",'\
+      body '{"gatewayEui":"GATEWAY_EUI","devAddr":"ANOTHER_DEVICE_EUI",'\
               '"packetTime":"2016-03-14T11:51:19.783817Z","tmst":3969010612,'\
               '"frequency":868.1,"dataRate":"SF12BW125","rssi":-120,'\
-              '"snr":-14.2,"data":"FfQAeAAZAF8ADQAEAE4CxA=="}'
+              '"snr":-14.2,"payload":"FfQAeAAZAF8ADQAEAE4CxA=="}'
     end
 
     trait :with_invalid_lora_data do
-      body '{"gatewayEui":"GATEWAY_EUI","deviceEui":"DEVICE_EUI",'\
+      body '{"gatewayEui":"GATEWAY_EUI","devAddr":"DEVICE_EUI",'\
               '"packetTime":"2016-03-14T11:51:19.783817Z","tmst":3969010612,'\
               '"frequency":868.1,"dataRate":"SF12BW125","rssi":-120,'\
-              '"snr":-14.2,"data":null}'
+              '"snr":-14.2,"payload":null}'
     end
 
     trait :via_wifi do
