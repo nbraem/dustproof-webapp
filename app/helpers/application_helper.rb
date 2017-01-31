3ca507830000b7ccb9578c4c9b1e72630b9b0f3e
Michael Smith <m@michaelsmith.be>
2017-01-31 12:37:34 +0100

    Hotfix: Prevent crash when determining dust level on account that has no measurements yet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,36 +13,40 @@ module ApplicationHelper
 
   def dust_level
     last_measurement = current_user.measurements.select(:pm25_ratio).order(timestamp: :desc).first
-    last_measurement.pm25_ratio
+    last_measurement.pm25_ratio if last_measurement
   end
 
   def dust_level_badge
     ratio = dust_level
-    if ratio < 1.0
-      content_tag(:span, 1, class: "label label-success")
-    elsif ratio.between?(1.0, 1.99)
-      content_tag(:span, 2, class: "label label-warning")
-    elsif ratio.between?(2.0, 3.99)
-      content_tag(:span, 3, class: "label label-warning")
-    elsif ratio.between?(4.0, 5.99)
-      content_tag(:span, 4, class: "label label-danger")
-    elsif ratio >= 6.0
-      content_tag(:span, 5, class: "label label-danger")
+    if ratio
+      if ratio < 1.0
+        content_tag(:span, 1, class: "label label-success")
+      elsif ratio.between?(1.0, 1.99)
+        content_tag(:span, 2, class: "label label-warning")
+      elsif ratio.between?(2.0, 3.99)
+        content_tag(:span, 3, class: "label label-warning")
+      elsif ratio.between?(4.0, 5.99)
+        content_tag(:span, 4, class: "label label-danger")
+      elsif ratio >= 6.0
+        content_tag(:span, 5, class: "label label-danger")
+      end
     end
   end
 
   def dust_level_color
     ratio = dust_level
-    if ratio < 1.0
-      "lime"
-    elsif ratio.between?(1.0, 1.99)
-      "orange"
-    elsif ratio.between?(2.0, 3.99)
-      "orange"
-    elsif ratio.between?(4.0, 5.99)
-      "red"
-    elsif ratio >= 6.0
-      "red"
+    if ratio
+      if ratio < 1.0
+        "lime"
+      elsif ratio.between?(1.0, 1.99)
+        "orange"
+      elsif ratio.between?(2.0, 3.99)
+        "orange"
+      elsif ratio.between?(4.0, 5.99)
+        "red"
+      elsif ratio >= 6.0
+        "red"
+      end
     end
   end
 end
