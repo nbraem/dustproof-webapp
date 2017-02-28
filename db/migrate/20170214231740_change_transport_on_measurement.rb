71414e069fd62561e8f094a2c97aae3082e63678
Michael Smith <m@michaelsmith.be>
2017-03-01 00:13:20 +0100

    Cleanup: Deleted obsolete migrations

--- db/migrate/20170214231740_change_transport_on_measurement.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTransportOnMeasurement < ActiveRecord::Migration
-  def change
-    change_column :measurements, :transport, :string, limit: 10
-  end
-end
--- db/migrate/20170228225917_remove_device_eui_and_api_key_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeviceEuiAndApiKeyFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :device_eui, :api_key
-  end
-end
