1b56f5b5f463134179b22015a364ef03e22e5cda
Nicolas Braem <nicolas.braem@gmail.com>
2017-03-15 22:14:08 +0100

    create vagrant db user

--- provision/deploy_postgres.sh
+++ provision/deploy_postgres.sh
@@ -2,5 +2,4 @@
 
 apt-get install -q=2 postgresql postgresql-client postgresql-contrib libpq-dev
 
-# TODO: ask Michael how he sets up postgres
-#sudo -u postgres createuser -s dustproof
+sudo -u postgres createuser --superuser vagrant
